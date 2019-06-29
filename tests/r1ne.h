e141e457d242b7ab900eaa91a4b11c26c4943b00
deegemon <49033712+deegemon@users.noreply.github.com>
2019-06-29 17:26:40 +0300

    Create r1ne.h

---
+++ tests/r1ne.h
@@ -0,0 +1,79 @@
+#ifndef R1NE_H
+#define R1NE_H
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "common.h"
+#include "text/_text.h"
+}
+
+TEST(r1ne,pr1){
+    text txt = create_text();
+    FILE *file;
+    char buf[MAXLINE + 1];
+    char loadFile[] = "test_save_input1.txt";
+    int index = 0;
+    int count = 0;
+
+    load(txt,loadFile);
+    remove_first_no_entry_line(txt);
+    node *current = txt->begin;
+
+
+    if((file = fopen("test_save_input1.txt","r")) == NULL){
+        FAIL();
+    }
+
+    while (fgets(buf,MAXLINE,file)) {
+        if(current && index  > 0){
+            ASSERT_STREQ(buf, current->contents);
+
+            count++;
+
+            current = current->next;
+        }
+        index++;
+    }
+
+    EXPECT_EQ(count,7);
+
+    fclose(file);
+
+    SUCCEED();
+}
+
+TEST(r1ne,pr2){
+    text txt = create_text();
+    FILE *file;
+    char buf[MAXLINE + 1];
+    char loadFile[] = "test_save_input2.txt";
+    int index = 0;
+    int count = 0;
+
+    load(txt,loadFile);
+    remove_first_no_entry_line(txt);
+    node *current = txt->begin;
+    current = current->next;
+
+    if((file = fopen("test_r1ne.txt","r")) == NULL){
+        FAIL();
+    }
+
+    while (fgets(buf,MAXLINE,file)) {
+        if(current && index  > 0){
+            ASSERT_STREQ(buf, current->contents);
+
+            count++;
+
+            current = current->next;
+        }
+        index++;
+    }
+
+    EXPECT_EQ(count,7);
+
+    fclose(file);
+
+    SUCCEED();
+}
+#endif // R1NE_H
