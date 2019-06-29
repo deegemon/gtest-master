8397fb4a2c10dc34f94e944b6355789eee69d8a8
deegemon <49033712+deegemon@users.noreply.github.com>
2019-06-29 23:17:08 +0300

    Update showupfirst.h

--- tests/showupfirst.h
+++ tests/showupfirst.h
@@ -10,23 +10,23 @@ TEST(showupfirst,pr1){
     text txt = create_text();
     FILE *file;
     char buf[MAXLINE + 1];
-    char loadFile[] = "tests/input/test_save_input1.txt";
-
+    char loadFile[] = "test_save_input3.txt";
     load(txt,loadFile);
     showupfirst(txt);
     node *current = txt->begin;
 
-    if((file = fopen("tests/result/test_showupfirst.txt","r")) == NULL){
+    if((file = fopen("test_suf.txt","r")) == NULL){
         FAIL();
     }
-
     while(fgets(buf,MAXLINE,file)){
         if(current){
-            ASSERT_STREQ(buf,current->contents);
+            EXPECT_STREQ(buf,current->contents);
 
             current = current->next;
         }
     }
+
+    fclose(file);
     SUCCEED();
 }
 #endif // SHOWUPFIRST_H
