748cff1411c3f3768bd4fc16320f556a3c14acfc
deegemon <49033712+deegemon@users.noreply.github.com>
2019-06-29 17:50:03 +0300

    Update r1ne.h

--- tests/r1ne.h
+++ tests/r1ne.h
@@ -53,7 +53,6 @@ TEST(r1ne,pr2){
     load(txt,loadFile);
     remove_first_no_entry_line(txt);
     node *current = txt->begin;
-    current = current->next;
 
     if((file = fopen("tests/result/test_r1ne.txt","r")) == NULL){
         FAIL();
